d651db5c1aca4bfe0e6259e11af48eec7fb4b68d
Fernando Liborio <fernando.liborio@datainfo.inf.br>
2018-12-21 17:03:08 -0300

    Setup of Spring boot deployable in external application server as WAR

---
+++ src/main/java/br/com/leuras/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package br.com.leuras.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.x509()
+			.subjectPrincipalRegex("CN=(.*?)(?:,|$)")
+			.userDetailsService(userDetailsService());
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsService() {
+			
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+			}
+		};
+	}
+}
