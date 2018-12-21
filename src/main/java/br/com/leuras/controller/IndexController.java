95ab3d830f9cec74027ed264a4f631b994be9704
Fernando Liborio <fernando.liborio@datainfo.inf.br>
2018-12-21 16:47:15 -0300

    Setup of Spring boot deployable in external application server as WAR

--- src/main/java/br/com/leuras/controller/IndexController.java
+++ src/main/java/br/com/leuras/controller/IndexController.java
@@ -1,14 +1,25 @@
 package br.com.leuras.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 	
+	@PreAuthorize("hasAuthority('ROLE_USER')")
 	@RequestMapping(value = {"/"}, method = RequestMethod.GET)
-	public String index() {
+	public String index(Model model, Principal principal) {
+		
+		final UserDetails user = (UserDetails) ((Authentication) principal).getPrincipal();
+		model.addAttribute("username", user.getUsername());
+		
 		return "index";
 	}
 }
